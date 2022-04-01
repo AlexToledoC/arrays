4f1fe44ab4524f8eb6988375cc2f40fe95636eec
Alejandro Toledo Cuenca <alejandrotoledocuenca@gmail.com>
2022-03-31 18:01:51 -0600

    Exito con array para las reservaciones

--- hotelBoutique.cpp
+++ hotelBoutique.cpp
@@ -1,7 +1,7 @@
 /* Ejercicio: hotel boutique
 Autor: Alejandro Toledo Cuenca	
-Fecha de creación: 30/03/2022
-Descripción: simular las reservaciones del hotel boutique */
+Fecha de creaciï¿½n: 30/03/2022
+Descripciï¿½n: simular las reservaciones del hotel boutique */
 
 #include <conio.h>
 #include <iostream>
@@ -10,14 +10,14 @@ Descripción: simular las reservaciones del hotel boutique */
 
 using namespace std; 
 int main(), i, seleccion, personas, habitacion, funcion_cobro(), doble[3],
-subtotal, dias, total, sencilla[5],
+subtotal, dias, total, sencilla[5], lujo[2],
 contador_sencilla, contador_doble, contador_lujo, funcion_otra_reserva();//funcion_reserva();
 double iva;
 string otra_operacion;
 
 int funcion_cobro(int subtotal)
 {
-	cout << "Cuantos días va a reservar? \n";
+	cout << "Cuantos dÃ­as va a reservar? \n";
 	cin >> dias;
 	cout << "Subtotal a pagar: $" << subtotal << "\n";
 	iva = subtotal * .16;
@@ -49,73 +49,76 @@ int main()
 	printf("1. Sencilla (maximo 2 personas).............$2,350.00 \n");
 	printf("2. Doble (maximo 4 personas)................$3,930.00 \n");
 	printf("3. De lujo (maximo 6 personas)..............$9,500.00 \n");
-	cout << "Escoja el tipo de habitación ingresando el respecitvo número (1-3):\n";
+	cout << "Escoja el tipo de habitacion ingresando el respecitvo nÃºmero (1-3):\n";
 	cin >> seleccion;
 	switch(seleccion)
 	{
 		case 1:
 			{
-				if (contador_sencilla > 4)
+				if (sencilla[4] == 1)
 				{
 					printf("Ya no hay habitaciones disponibles.");
 				}
 				else
 				{
-					cout << "¿Para cuantas personas es la reservación? \n";
+					cout << "Â¿Para cuantas personas es la reservacion? \n";
 					cin >> personas;
 					if (personas > 2 or personas < 1)
 					{
-						printf("Esta habitación no admite ese número de personas.");
+						printf("Esta habitacion no admite ese numero de personas.");
 					}
 					else
 					{
 						funcion_cobro(2350);
 					}
 				}
+				sencilla[contador_sencilla] = 1;
 				contador_sencilla += 1;
 				break;
 			}
 		case 2:
 			{
-				if (contador_doble > 3)
+				if (doble[3] == 1)
 				{
 					printf("Ya no hay habitaciones disponibles.");
 				}
 				else
 				{
-					cout << "¿Para cuantas personas es la reservación? \n";
+					cout << "Para cuantas personas es la reservacion? \n";
 					cin >> personas;
 					if (personas > 4 or personas < 1)
 					{
-						printf("Esta habitación no admite ese número de personas.");
+						printf("Esta habitacin no admite ese numero de personas.");
 					}
 					else
 					{
 						funcion_cobro(3930);
 					}
 				}
+				doble[contador_doble] = 1;
 				contador_doble += 1;
 				break;
 			}
 		case 3:
 			{
-				if (contador_lujo > 1)
+				if (lujo[1] == 1)
 				{
 					printf("Ya no hay habitaciones disponibles.");
 				}
 				else
 				{
-					cout << "¿Para cuantas personas es la reservación? \n";
+					cout << "Â¿Para cuantas personas es la reservaciÃ³n? \n";
 					cin >> personas;
 					if (personas > 6 or personas < 1)
 					{
-						printf("Esta habitación no admite ese número de personas.");
+						printf("Esta habitaciÃ³n no admite ese nÃºmero de personas.");
 					}
 					else
 					{
 						funcion_cobro(9500);
 					}
 				}
+				lujo[contador_lujo] = 1;
 				contador_lujo += 1;
 				break;
 		default:
